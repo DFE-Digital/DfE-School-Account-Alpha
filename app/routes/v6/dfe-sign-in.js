0dede88533e4d84f0111fd76344536815ceab303
Kelly Millward <kelly.robinson@education.gov.uk>
2026-08-12 15:24:16 +0100

    Fix prototype routing
    Fix routing for the census details journey to switch between single and multi view
    Fix routing for older screens, such as single-priority-home, which were defaulting to census-details
    Test all affected routes and navigation paths
    Updated index page to pull in jira tickets and simplified current and older screens in V6

--- app/routes/v6/dfe-sign-in.js
+++ app/routes/v6/dfe-sign-in.js
@@ -1,25 +1,47 @@
 module.exports = function (router) {
+  
+  var version = "v6"
 
-  var version = "v6";
+  function setLocalAuthority(req) {
+    req.session.data['userType'] = 'localAuthority'
+    delete req.session.data['singleHome']
+  }
+
+  function setSingleHome(req) {
+    if (req.query.singleHome) {
+      req.session.data['singleHome'] = req.query.singleHome
+      delete req.session.data['userType']
+    }
+  }
 
   router.get('/' + version + '/emails/local-authority-census-open', function (req, res) {
+    setLocalAuthority(req)
     res.render(version + '/emails/local-authority-census-open', {})
   })
 
   router.post('/' + version + '/emails/local-authority-census-open', function (req, res) {
+    setLocalAuthority(req)
     res.redirect('/' + version + '/sign-in')
   })
 
   router.get('/' + version + '/emails/local-authority-census-action-required', function (req, res) {
+    setLocalAuthority(req)
     res.render(version + '/emails/local-authority-census-action-required', {})
   })
 
   router.post('/' + version + '/emails/local-authority-census-action-required', function (req, res) {
+    setLocalAuthority(req)
     req.session.data['censusStatus'] = 'actionRequired'
     res.redirect('/' + version + '/sign-in')
   })
 
+  router.get('/' + version + '/emails/update-email', function (req, res) {
+    setSingleHome(req)
+    res.render(version + '/emails/update-email', {})
+  })
+
   router.get('/' + version + '/sign-in', function (req, res) {
+    setSingleHome(req)
     res.render(version + '/sign-in', {})
   })
 
@@ -40,13 +62,26 @@ module.exports = function (router) {
   })
 
   router.post('/' + version + '/mfa', function (req, res) {
-    if (req.session.data['singleHome'] == 'thinLine') {
+    if (req.session.data['userType'] == 'localAuthority') {
+      delete req.session.data['singleHome']
       res.redirect('/' + version + '/compliance/census-details')
-    } else if (req.session.data['userType'] == 'localAuthority') {
+    } else if (req.session.data['singleHome'] == 'thinLine') {
+      delete req.session.data['userType']
       res.redirect('/' + version + '/compliance/census-details')
+    } else if (req.session.data['singleHome'] == 'combined') {
+      delete req.session.data['userType']
+      res.redirect('/' + version + '/single-combined-home')
+    } else if (req.session.data['singleHome'] == 'priority') {
+      delete req.session.data['userType']
+      res.redirect('/' + version + '/single-priority-home')
+    } else if (req.session.data['singleHome'] == 'nowNextLater') {
+      delete req.session.data['userType']
+      res.redirect('/' + version + '/single-now-next-later-home')
+    } else if (req.session.data['singleHome'] == 'roleLed') {
+      delete req.session.data['userType']
+      res.redirect('/' + version + '/single-role-led-home')
     } else {
       res.redirect('/' + version + '/single-school')
     }
   })
-
 }
